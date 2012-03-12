e6f2283934d9168a4fa888eeabcd6452bc053c4b
Qark <qark@ospace.net>
2012-03-12 12:46:37 +0100

    Initial commit

---
+++ src/net/ospace/openhouse/OpenHouseActivity.java
@@ -0,0 +1,13 @@
+package net.ospace.openhouse;
+
+import android.os.Bundle;
+import com.phonegap.DroidGap;
+
+public class OpenHouseActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
