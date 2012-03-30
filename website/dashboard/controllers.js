247e4c7fb580f6b7a73ff482f4a58677ac2c6254
Qark <qark@ospace.net>
2012-03-30 10:40:09 +0200

    Dashboard and mobile web initial commit

---
+++ website/dashboard/controllers.js
@@ -0,0 +1,67 @@
+function ScheduleCtrl($xhr, $defer) {
+  var self = this;
+
+  // used for debugging
+  var hourOffset = 0; //12 - (new Date().getMinutes());
+
+  self.response = null;
+  self.currentTime = "--:--";
+  self.eventBlocks = [];
+
+  $xhr("GET", "schedule.json", function(code, response) {
+    self.response = response;
+    self.update();
+  });
+
+  // periodically update page
+  self.update = function() {
+    var now = new Date();
+    // debug only
+    if (0) {
+      var hours = now.getMinutes() + hourOffset;
+      var minutes = now.getSeconds();
+      now.setDate(4);
+      now.setMonth(3);
+      now.setHours(hours);
+      now.setMinutes(minutes);
+    }
+
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    self.currentTime = hours + ":" + minutes;
+
+    var nowMinus10 = now.getTime() / 1000 - 600; // now - 10 minutes in seconds
+
+    // reset events
+    self.eventBlocks = [];
+    // group events by time
+    var lastTime = "";
+    var lastGroup = [];
+    var groupCount = 0;
+    angular.forEach(self.response.items, function(value, key) {
+      if (value.timestamp < nowMinus10) {
+        // past event, ignore it
+      } else if (value.start != lastTime) {
+        if (lastGroup.length > 0 && groupCount < 2) {
+          self.eventBlocks.push({"start": lastTime, "events": lastGroup});
+          groupCount += 1;
+        };
+        lastTime = value.start;
+        lastGroup = [value];
+      } else {
+        lastGroup.push(value);
+      }
+    });
+
+    $defer(self.update, 1000);
+  }
+
+  $defer(self.update, 1000);
+
+}
+
+//ScheduleCtrl.$inject = ['$xhr'];
+
